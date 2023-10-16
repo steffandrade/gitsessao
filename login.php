44d8e281ff369b551abf3c35195e0e8c91b7795e
stefanyaandrade <stefany.andrade@portalsesisp.org.br>
2023-10-16 10:56:39 -0300

    Commit: Criação de Tela de Login em php

--- login.php
+++ login.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
+|<!DOCTYPE html>
 <html lang="pt-BR">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SESSÃO</title>
+    <title>Tela de Login</title>
 </head>
 <body>
-   
-<h1>Teste - 01</h1>
+    
+<h1>Tela de Login</h1>
 
 </body>
 </html>
